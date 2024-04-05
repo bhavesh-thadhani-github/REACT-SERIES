acd156319b80662b8608a4ceab67c5c9fca8bdaf
bhavesh-thadhani-github <152782056+bhavesh-thadhani-github@users.noreply.github.com>
2024-04-05 17:17:47 +0530

    Update App.jsx

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -55,11 +55,11 @@ function App() {
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
-            {todos.map((todo) => {
+            {todos.map((todo) => (
               <div key={todo.id} className="w-full">
                 <TodoItem todo={todo}/>
               </div>
-            })}
+            ))}
           </div>
         </div>
       </div>
